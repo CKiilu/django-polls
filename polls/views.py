7fc3bf12a5e0045240c74475be634a84f9147915
CKiilu <cndeti@gmail.com>
2016-02-12 17:27:24 +0300

    Form view

--- polls/views.py
+++ polls/views.py
@@ -15,7 +15,23 @@ def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html',{'question': question})
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay voting form
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return HTTPResponseRedirect after
+		# successfully dealing with POST data to 
+		# prevent data from being posted twice if 
+		# a user hits the back button
+		return HttpResponseRedirect(reverse('polls:results', args = question.id,))
+
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
